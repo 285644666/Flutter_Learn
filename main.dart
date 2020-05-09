5827c19a65db8c22e3467e700619aa4f11306a12
Tyler.JIANG <38577762+285644666@users.noreply.github.com>
2020-05-10 02:42:47 +0900

    创建居中文本

---
+++ main.dart
@@ -0,0 +1,14 @@
+// This is my first app.
+
+import 'package:flutter/material.dart';
+
+//The main function is the starting point for all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(   //位于中心
+        child: Text('Hello World',
+      ),
+    ),
+  );
+}
